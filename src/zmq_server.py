919bbce5833f5549e7575eea4ec364c14aa7ddbb
riowei <Mario.Weisz@gmail.com>
2023-08-14 19:52:19 -0700

    Create zmq_server.py

---
+++ src/zmq_server.py
@@ -0,0 +1,52 @@
+import argparse
+
+desc = 'A Python program that performs tasks for virtuoso.'
+parser = argparse.ArgumentParser(description=desc)
+parser.add_argument('port', type=int, help='socket port number.')
+args = parser.parse_args()
+port = args.port
+
+# import zmq
+
+# context = zmq.Context()
+# socket = context.socket(zmq.REP)
+# socket.bind(f"tcp://127.0.0.1:{port}")
+
+# message = socket.recv_json()
+# print("Received request: %s" % message)
+
+# socket.close()
+# context.term()
+
+##########################################################
+
+import os
+import sys
+import bag
+
+def run_skill_server(port):
+    """Run the BAG/Virtuoso server."""
+    error_msg = ''
+    server = None
+    
+    try:
+        # attempt to open port and start server
+        router = bag.interface.ZMQRouter(port=port)
+        server = bag.interface.SkillServer(router, sys.stdout, sys.stdin, tmpdir=None)
+
+    except Exception as ex:
+        error_msg = 'bag server process error:\n%s\n' % str(ex)
+
+    if not error_msg:
+        try:
+            sys.stdout.write('BAG skill server has started.  Yay!\n')
+            sys.stdout.flush()
+            server.run()
+        except Exception as ex:
+            error_msg = 'bag server process error:\n%s\n' % str(ex)
+
+    if error_msg:
+        sys.stderr.write(error_msg)
+        sys.stderr.flush()
+
+run_skill_server(port)
